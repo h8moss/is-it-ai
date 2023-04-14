ac2608e5516b37fc2b0c0f3057ffeeceebc04f9c
h8m0ss <danarmenta32@gmail.com>
2023-04-14 13:17:54 -0600

    added pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class="content roboto-font">
+<h1>Is it AI?</h1>
+<a href='/play' class="btn">Play</a>
+<a href='/about'>Learn more</a>
+</div>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap');
+
+  div.content {
+    display: flex;
+    flex-direction: column;
+    
+    width: 100%;
+    height: 100%;
+    padding: 1rem;
+
+    justify-content: space-around;
+    text-align: center;
+  }
+
+  h1 {
+    margin: 0;
+    font-size: 2.4rem;
+  }
+
+  a {
+    margin: 0px auto;
+  }
+
+  a.btn {
+    background-color: var(--hl-color);
+    color: white;
+
+    padding: 1.2rem 2rem;
+    font-size: 1.5rem;
+    border-radius: 1rem;
+
+    text-decoration: none;
+
+    transition-property: background-color;
+    transition-duration: 200ms;
+    transition-timing-function: ease-in-out
+  }
+
+  a.btn:hover {
+    background-color: var(--hl-color-darker);
+  }
+</style>
---
+++ src/routes/play/+page.svelte
@@ -0,0 +1,54 @@
+
+
+<script lang='ts'>
+
+</script>
+
+<div class='content roboto-font'>
+<h1>Which is AI?</h1>
+<div class='quotes'>
+  <div>QUOTE 1</div>
+  <div>QUOTE 2</div>
+</div>
+<footer>
+  <span>High score</span>
+  <span>score</span>
+</footer>
+</div>
+
+<style>
+  div.content {
+    --padding-x: 0px;
+    --padding-y: 2rem;
+
+    height: calc(100% - var(--padding-y)*2);
+    width: calc(100% - var(--padding-x)*2);
+    padding: var(--padding-y) var(--padding-x);
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    text-align: center;
+  }
+
+  div.quotes {
+    flex: 1;
+    display: flex;
+  }
+
+  div.quotes > div
+  {
+    flex: 1;
+    margin: auto;
+  }
+
+  footer {
+    display: flex;
+    justify-content: space-around;
+  }
+
+  footer > * {
+    margin: auto;
+  }
+</style>
